13b20f715cbd8a1d7bb44cc910ef232cec09c48f
Antonio <anthony.bold222@outlook.com>
2025-06-07 15:08:02 -0600

    Agregando exe_1.py a exercises

---
+++ exercises/exe_1.py
@@ -0,0 +1,28 @@
+##Type this in the console -check the type of objects##
+
+type(5)
+type(3.0)
+
+##Type this in the console - Convert to another type##
+
+float(3)
+int(3.9)
+round(3.9)
+
+##Type this in the console - Expressions ##
+3+2
+(4+2)*6-1
+type((4+2)*6-1)
+print("Resultado:", float((4+2)*6-1))
+
+#Type this in the console - Variables##
+pi = 355/113
+
+#Compute approximate value for pi
+pi= 355/133
+
+#Compute approximate value for pi
+pi=355/113
+radius=2.2
+area=pi*(radius**2)
+circumference=pi*(radius*2)
